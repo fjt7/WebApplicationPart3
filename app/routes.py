1b4329d250d459f836a7a0d324fe98122407a576
Franklin Tan <fjt7@njit.edu>
2021-04-25 22:59:27 -0400

    Feature: routes.py - Protecting app from unauthorized users

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user, login_required
+
+
+# Blueprint Configuration
+main_bp = Blueprint(
+    'main_bp', __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+
+@main_bp.route('/', methods=['GET'])
+@login_required
+def dashboard():
+    """Logged-in User Dashboard."""
+    return render_template(
+        'dashboard.jinja2',
+        title='Flask-Login Tutorial.',
+        template='dashboard-template',
+        current_user=current_user,
+        body="You are now logged in!"
+    )
