b80859200eabd8201e54401966d2f50c923f142d
Franklin Tan <fjt7@njit.edu>
2021-04-25 22:57:47 -0400

    Feature: load_user and unauthorized handlers

--- app/auth.py
+++ app/auth.py
@@ -66,3 +66,17 @@ def login():
         template='login-page',
         body="Log in with your User account."
     )
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Check if user is logged-in on every page load."""
+    if user_id is not None:
+        return User.query.get(user_id)
+    return None
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    """Redirect unauthorized users to Login page."""
+    flash('You must be logged in to view that page.')
+    return redirect(url_for('auth_bp.login'))
